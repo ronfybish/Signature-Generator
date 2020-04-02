948155331f7325c6eede137b27a1b620e19f8fa8
ron fybish <fybishron@gmail.com>
2020-04-02 10:56:19 +0300

    adding youtube section

--- my-app/src/component/AppHeading.js
+++ my-app/src/component/AppHeading.js
@@ -11,7 +11,7 @@ export default class AppJumbotron extends Component {
                 Just create your signature, and copy & paste to your email account
                 </p>
                 <p className='text-center'>
-                <a href="https://www.producthunt.com/posts/just-sign-2?utm_source=badge-top-post-badge&utm_medium=badge&utm_souce=badge-just-sign-2" target="_blank"><img src="https://api.producthunt.com/widgets/embed-image/v1/top-post-badge.svg?post_id=189309&theme=light&period=daily" alt="Just Sign - Build your email signature in less than 60 seconds | Product Hunt Embed" style={{width:"250px", height: "54px"}} width="250px" height="54px" /></a>
+                <a href="https://www.producthunt.com/posts/just-sign-2?utm_source=badge-top-post-badge&utm_medium=badge&utm_souce=badge-just-sign-2" target="_blank" rel="noopener noreferrer"><img src="https://api.producthunt.com/widgets/embed-image/v1/top-post-badge.svg?post_id=189309&theme=light&period=daily" alt="Just Sign - Build your email signature in less than 60 seconds | Product Hunt Embed" style={{width:"250px", height: "54px"}} width="250px" height="54px" /></a>
                 </p>
               </Container>
           </div>
